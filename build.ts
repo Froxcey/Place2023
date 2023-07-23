6d1e680ef170155c54cef650bfc254790c8f417c
froxcey <danichen204@gmail.com>
2023-07-23 19:52:12 +0800

    Update coords

--- build.ts
+++ build.ts
@@ -7,7 +7,7 @@ let input = await Bun.file("placeAzzy-source.user.js").text();
 var obfuscationResult = Obfuscator.obfuscate(input, {
   optionsPreset: "medium-obfuscation",
 }).getObfuscatedCode();
-let output = input.split("// Begin")[0] + obfuscationResult;
+let output = input.split("// Begin")[0] + obfuscationResult + "\n";
 Bun.write("placeAzzy.user.js", output);
 
 let version = input.split("// @version")[1].split("\n")[0].trim();
