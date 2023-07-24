40023ed569b145e153ea6dbcf9f989480d4f1cf9
froxcey <danichen204@gmail.com>
2023-07-24 12:39:44 +0800

    New template

---
+++ placeAzzy_azzy-bg.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Azzy Bot - Azzy Bg
+// @homepage     https://discord.gg/AmUVTBST
+// @updateURL    https://github.com/Froxcey/Place2023/raw/master/placeAzzy_azzy-bg.user.js
+// @downloadURL  https://github.com/Froxcey/Place2023/raw/master/placeAzzy_azzy-bg.user.js
+// @version      1.0
+// @description  try to take over the world!
+// @author       Froxcey
+// @match        https://garlic-bread.reddit.com/embed*
+// @connect      githubusercontent.com
+// ==/UserScript==
+
+window.botTasks = {
+  azzyBg: true,
+};
